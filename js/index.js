6935732a8705c60099b113eaa60989f1d9bfb652
Mira Rakymzhan <miratulebaeva@gmail.com>
2023-01-29 19:22:34 +0600

    burger menu was added

--- js/index.js
+++ js/index.js
@@ -5,12 +5,24 @@ let portfolioBtns = document.querySelector('.box');
 let portfolioImages = document.querySelectorAll ('.pictures-pic');
 let languages = document.querySelector('.languages');
 let lngs =document.querySelectorAll('.language');
+let night_mode = document.querySelector('.night_mode');
+let burgerMenu = document.querySelector('.burger');
+let nav = document.querySelector('.nav');
+let navClose = document.querySelector('.header-nav__close')
 
 // portfolioBtn.addEventListener('click', function() {
 // portfolioImages.forEach((image, index) => {
 //      image.src = `assets/JPG/winter/${index + 1}.jpg`
 // })
 // });
+
+function openMenu() {
+    nav.classList.add('nav-menu_active');
+}
+ 
+function closeMenu() {
+    nav.classList.remove('nav-menu_active');
+}
 
 function changeImage(event) {
     if (event.target.classList.contains('portfolio-btn')) {
@@ -65,16 +77,30 @@ function changeActiveForLngs() {
         })
     }
 
+function changeMode() {
+    const sections = document.querySelectorAll('.skills, .portfolio, .video, .prices, .main');
+    const sectionHeader = document.querySelectorAll('.section-header');
+    const sectionTitle = document.querySelectorAll('.section-title');
+
+    sections.forEach(section => section.classList.toggle('light-theme'));
+    sectionHeader.forEach(header => header.classList.toggle('light-theme'));
+    sectionTitle.forEach(title => title.classList.toggle('light-theme'))
+}
 
 
 
 // languages[1].addEventListener('click', getTranslate('ru'))
 // getTranslate('ru')
 
-languages.addEventListener('click', getLanguage)
+burgerMenu.addEventListener('click',openMenu);
+navClose.addEventListener('click',closeMenu);
+languages.addEventListener('click', getLanguage);
 changeActiveForLngs();
 changeActiveForBtns();
-portfolioBtns.addEventListener('click', changeImage)
+portfolioBtns.addEventListener('click', changeImage);
+night_mode.addEventListener('click', changeMode);
+
+
 
 // console.log(portfolioBtn)
 // console.log(portfolioImages)
